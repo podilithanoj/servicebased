7df8b9c987c472baa0879873357438148d9377a3
podilithanoj <psn.thanoj@gmail.com>
2025-04-17 15:38:00 +0530

    added

--- terraform/aws123.network/java-app-network-2/envs/dev/backend.tf
+++ terraform/aws123.network/java-app-network-2/envs/dev/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "glacade"
-    key            = "javanetwork/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket = "glacade"
+    key    = "javanetwork/dev/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
   }
 }
